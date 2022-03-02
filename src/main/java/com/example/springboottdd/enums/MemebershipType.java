784a9b774afbcecd8fa5a82c3c46b8fa073c6bb5
이승환 <lsh01087674117@gmail.com>
2022-03-02 22:22:14 +0900

    네이버, 라인, 카카오 추가.

--- src/main/java/com/example/springboottdd/enums/MemebershipType.java
+++ src/main/java/com/example/springboottdd/enums/MemebershipType.java
@@ -1,8 +1,19 @@
 package com.example.springboottdd.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author 이승환
  * @since 2022-03-02
  */
+@Getter
+@RequiredArgsConstructor
 public enum MemebershipType {
+
+    NAVER("네이버"),
+    LINE("라인"),
+    KAKAO("카카오");
+
+    private final String companyName;
 }
