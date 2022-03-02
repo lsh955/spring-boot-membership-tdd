bc715511d1d7289489627bd6c45bdea17a90646e
이승환 <lsh01087674117@gmail.com>
2022-03-02 22:11:27 +0900

    멤버십 등록 테스트코드 작성.

--- src/test/java/com/example/springboottdd/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/springboottdd/repository/MembershipRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.springboottdd.repository;
 
+import com.example.springboottdd.domain.Membership;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,22 @@ public class MembershipRepositoryTest {
     @Autowired
     private MembershipRepository membershipRepository;
 
+    @Test
+    public void 멤버십등록() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
 
+        // when
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        Assertions.assertThat(result.getId()).isNotNull();
+        Assertions.assertThat(result.getUserId()).isEqualTo("userId");
+        Assertions.assertThat(result.getMembershipName()).isEqualTo("네이버");
+        Assertions.assertThat(result.getPoint()).isEqualTo(10000);
+    }
 }
