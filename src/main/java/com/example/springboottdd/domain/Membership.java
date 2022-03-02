09cb4936cdb27584243b7709e498c7edc1887e3c
이승환 <lsh01087674117@gmail.com>
2022-03-02 22:13:54 +0900

    테스트코드 에러를 해결하기위한 Membership 프로덕션 코드 추가.

--- src/main/java/com/example/springboottdd/domain/Membership.java
+++ src/main/java/com/example/springboottdd/domain/Membership.java
@@ -1,9 +1,16 @@
 package com.example.springboottdd.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * @author 이승환
@@ -11,6 +18,9 @@ import javax.persistence.*;
  */
 @Entity
 @Table
+@Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Membership {
 
@@ -18,4 +28,21 @@ public class Membership {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 20)
+    private String membershipName;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer point;
+
+    @CreationTimestamp
+    @Column(nullable = false, length = 20, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(length = 20)
+    private LocalDateTime updatedAt;
 }
