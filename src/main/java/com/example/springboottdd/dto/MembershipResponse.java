4c0d4de2ddf9feafe4e61094648c4f92dfba194f
이승환 <lsh01087674117@gmail.com>
2022-03-03 22:23:25 +0900

    MembershipResponse DTO 추가.

---
+++ src/main/java/com/example/springboottdd/dto/MembershipResponse.java
@@ -0,0 +1,19 @@
+package com.example.springboottdd.dto;
+
+import com.example.springboottdd.enums.MembershipType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author 이승환
+ * @since 2022-03-03
+ */
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class MembershipResponse {
+
+    private final Long id;
+    private final MembershipType membershipType;
+}
