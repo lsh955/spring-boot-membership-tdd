486caa1c69b755cad30b9c15c67a30d9e308c1d5
이승환 <lsh01087674117@gmail.com>
2022-03-03 19:20:39 +0900

    멤버십이 이미 존재하는 테스트코드 추가.

--- src/test/java/com/example/springboottdd/service/MembershipServiceTest.java
+++ src/test/java/com/example/springboottdd/service/MembershipServiceTest.java
@@ -1,6 +1,12 @@
 package com.example.springboottdd.service;
 
+import com.example.springboottdd.domain.Membership;
+import com.example.springboottdd.enums.MemebershipType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
@@ -10,5 +16,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class MembershipServiceTest {
 
+    private final String userId = "userId";
+    private final MemebershipType memebershipType = MemebershipType.NAVER;
+    private final Integer point = 10000;
 
+    @Test
+    @DisplayName("멤버십등록실패_이미존재함")
+    public void 멤버십등록실패_이미존재함() {
+        // given
+        Mockito.doReturn(Membership.builder().build()).when(memebershipType).findByUserIdAndMembershipType(userId, memebershipType);
+
+        // when
+        final MembershipException result = Assertions.assertThrows(MembershipErrorResult.class, () -> target.addMembership(userId, memebershipType, point));
+
+        // then
+        org.assertj.core.api.Assertions.assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
+    }
 }
