789bd0d7d7c0cb8d748b3f4eb39a8bce83c64b31
이승환 <lsh01087674117@gmail.com>
2022-03-03 19:57:40 +0900

    컴파일 에러 해결.

--- src/test/java/com/example/springboottdd/service/MembershipServiceTest.java
+++ src/test/java/com/example/springboottdd/service/MembershipServiceTest.java
@@ -2,16 +2,19 @@ package com.example.springboottdd.service;
 
 import com.example.springboottdd.domain.Membership;
 import com.example.springboottdd.enums.MembershipErrorResult;
+import com.example.springboottdd.enums.MembershipType;
 import com.example.springboottdd.exception.MembershipException;
 import com.example.springboottdd.repository.MembershipRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
 
 /**
  * @author 이승환
@@ -26,20 +29,20 @@ public class MembershipServiceTest {
     @Mock
     private MembershipRepository membershipRepository;
 
-//    private final String userId = "userId";
-//    private final MemebershipType memebershipType = MemebershipType.NAVER;
-//    private final Integer point = 10000;
+    private final String userId = "userId";
+    private final MembershipType membershipType = MembershipType.NAVER;
+    private final Integer point = 10000;
 
     @Test
     @DisplayName("멤버십등록실패_이미존재함")
     public void 멤버십등록실패_이미존재함() {
         // given
-        Mockito.doReturn(Membership.builder().build()).when(memebershipType).findByUserIdAndMembershipType(userId, memebershipType);
+        doReturn(Membership.builder().build()).when(membershipRepository).findByUserIdAndMembershipType(userId, membershipType);
 
         // when
-        final MembershipException result = Assertions.assertThrows(MembershipErrorResult.class, () -> target.addMembership(userId, memebershipType, point));
+        final MembershipException result = assertThrows(MembershipException.class, () -> target.addMembership(userId, membershipType, point));
 
         // then
-        org.assertj.core.api.Assertions.assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
     }
 }
