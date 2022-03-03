b9bdb7a0a5bc5ea87beb7156965059196cfe7e13
이승환 <lsh01087674117@gmail.com>
2022-03-03 19:42:51 +0900

    MembershipException Class 에 대한 주석추가.

--- src/main/java/com/example/springboottdd/exception/MembershipException.java
+++ src/main/java/com/example/springboottdd/exception/MembershipException.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 /**
  * @author 이승환
  * @since 2022-03-03
+ *
+ * MembershipException이 throw 되었을 때 RestControllerAdvice를 통해 MembershipErrorResult의 HttpStatus와 message를 반환하기 위함
  */
 @Getter
 @RequiredArgsConstructor
