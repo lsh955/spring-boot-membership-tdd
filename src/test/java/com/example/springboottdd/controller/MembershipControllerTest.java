8ad39b5e11cc101503e9bc3e4c242876acc790c1
이승환 <lsh01087674117@gmail.com>
2022-03-04 21:12:34 +0900

    DisplayName 추가.

--- src/test/java/com/example/springboottdd/controller/MembershipControllerTest.java
+++ src/test/java/com/example/springboottdd/controller/MembershipControllerTest.java
@@ -2,6 +2,7 @@ package com.example.springboottdd.controller;
 
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,6 +32,7 @@ public class MembershipControllerTest {
     }
 
     @Test
+    @DisplayName("mockMvc가 Null인지 여부를 판단")
     public void mockMvc가Null이아님() throws Exception {
         assertThat(membershipController).isNotNull();
         assertThat(mockMvc).isNotNull();
