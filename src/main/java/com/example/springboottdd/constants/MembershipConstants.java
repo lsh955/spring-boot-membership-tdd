d113781e9ba12a153be588faee9490b38579215d
이승환 <lsh01087674117@gmail.com>
2022-03-05 17:04:57 +0900

    멤버십등록실패_사용자식별값이헤더에없음() 프로덕션 코드추가

---
+++ src/main/java/com/example/springboottdd/constants/MembershipConstants.java
@@ -0,0 +1,14 @@
+package com.example.springboottdd.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 이승환
+ * @since 2022-03-05
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MembershipConstants {
+
+    public final static String USER_ID_HEADER = "X-USER-ID";
+}
--- src/main/java/com/example/springboottdd/controller/MembershipController.java
+++ src/main/java/com/example/springboottdd/controller/MembershipController.java
@@ -1,6 +1,15 @@
 package com.example.springboottdd.controller;
 
+import com.example.springboottdd.dto.MembershipRequest;
+import com.example.springboottdd.dto.MembershipResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.springboottdd.constants.MembershipConstants.USER_ID_HEADER;
 
 /**
  * @author 이승환
@@ -8,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MembershipController {
+
+    @PostMapping("/api/v1/membership")
+    public ResponseEntity<MembershipResponse> addMembership (
+            @RequestHeader(USER_ID_HEADER) final String userid,
+            @RequestBody final MembershipRequest membershipRequest ) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
