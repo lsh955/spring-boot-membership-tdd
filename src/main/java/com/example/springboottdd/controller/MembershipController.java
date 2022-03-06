7706f84d4451c0a1742d8935763ef49e3bb36416
이승환 <lsh01087674117@gmail.com>
2022-03-06 09:56:05 +0900

    테스트 에러에 대한 MembershipController 프로덕션 코드 추가(controller에서 memberService를 이용하지 않기 때문에 생긴에러)

--- src/main/java/com/example/springboottdd/controller/MembershipController.java
+++ src/main/java/com/example/springboottdd/controller/MembershipController.java
@@ -2,6 +2,8 @@ package com.example.springboottdd.controller;
 
 import com.example.springboottdd.dto.MembershipRequest;
 import com.example.springboottdd.dto.MembershipResponse;
+import com.example.springboottdd.service.MembershipService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +20,17 @@ import static com.example.springboottdd.constants.MembershipConstants.USER_ID_HE
  * @since 2022-03-04
  */
 @RestController
+@RequiredArgsConstructor
 public class MembershipController {
+
+    private final MembershipService membershipService;
 
     @PostMapping("/api/v1/membership")
     public ResponseEntity<MembershipResponse> addMembership (
             @RequestHeader(USER_ID_HEADER) final String userid,
             @RequestBody @Valid final MembershipRequest membershipRequest) {
+
+        membershipService.addMembership(userid, membershipRequest.getMembershipType(), membershipRequest.getPoint());
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
