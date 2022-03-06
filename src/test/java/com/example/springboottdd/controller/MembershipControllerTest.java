5e5f7073912cf58c77e25ccf8bb3ce9517205358
이승환 <lsh01087674117@gmail.com>
2022-03-06 09:42:32 +0900

    MemberService 에서의 MembershipException이 throw된 테스트 코드 작성.

--- src/test/java/com/example/springboottdd/controller/MembershipControllerTest.java
+++ src/test/java/com/example/springboottdd/controller/MembershipControllerTest.java
@@ -1,13 +1,17 @@
 package com.example.springboottdd.controller;
 
 import com.example.springboottdd.dto.MembershipRequest;
+import com.example.springboottdd.enums.MembershipErrorResult;
 import com.example.springboottdd.enums.MembershipType;
+import com.example.springboottdd.exception.MembershipException;
+import com.example.springboottdd.service.MembershipService;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static com.example.springboottdd.constants.MembershipConstants.USER_ID_HEADER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -28,6 +33,9 @@ public class MembershipControllerTest {
 
     @InjectMocks
     private MembershipController membershipController;
+
+    @Mock
+    private MembershipService membershipService;
 
     private MockMvc mockMvc;
     private Gson gson;
@@ -110,6 +118,27 @@ public class MembershipControllerTest {
                 MockMvcRequestBuilders.post(url)
                         .header(USER_ID_HEADER, "12345")
                         .content(gson.toJson(membershipRequest(10000, null)))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("맴버십등록 실패(MemberService 에서의 에러인 경우)")
+    public void 멤버십등록실패_MemberService에서에러Throw() throws Exception {
+        // given
+        final String url = "/api/v1/membership";
+        doThrow(new MembershipException(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER))
+                .when(membershipService)
+                .addMembership("12345", MembershipType.NAVER, 10000);
+
+        //when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .header(USER_ID_HEADER, "12345")
+                        .content(gson.toJson(membershipRequest(10000, MembershipType.NAVER)))
                         .contentType(MediaType.APPLICATION_JSON)
         );
 
