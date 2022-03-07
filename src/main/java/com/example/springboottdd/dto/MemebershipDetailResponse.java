9336c491e7d38e93b405954ce3c5a5ae60b20d36
이승환 <lsh01087674117@gmail.com>
2022-03-07 19:47:03 +0900

    필요 필드와 어노테이션 추가.

--- src/main/java/com/example/springboottdd/dto/MemebershipDetailResponse.java
+++ src/main/java/com/example/springboottdd/dto/MemebershipDetailResponse.java
@@ -1,8 +1,23 @@
 package com.example.springboottdd.dto;
 
+import com.example.springboottdd.enums.MembershipType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * @author 이승환
  * @since 2022-03-07
  */
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class MemebershipDetailResponse {
+
+    private final Long id;
+    private final MembershipType membershipType;
+    private final LocalDateTime createdAt;
+    private final Integer point;
 }
