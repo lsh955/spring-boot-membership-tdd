3b01e46f665d1472073beaa7d115459d447988ab
이승환 <lsh01087674117@gmail.com>
2022-03-07 19:29:59 +0900

    테스트 코드에서의 findAllByUserId() 컴파일 에러 해결을 위한 프로덕션 코드추가.

--- src/main/java/com/example/springboottdd/repository/MembershipRepository.java
+++ src/main/java/com/example/springboottdd/repository/MembershipRepository.java
@@ -4,6 +4,8 @@ import com.example.springboottdd.domain.Membership;
 import com.example.springboottdd.enums.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author 이승환
  * @since 2022-03-01
@@ -12,4 +14,5 @@ public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 
+    List<Membership> findAllByUserId(final String userId);
 }
