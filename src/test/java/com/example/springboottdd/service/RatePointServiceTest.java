6a12c7185f763c2de76ca867e669df22b8868f1b
이승환 <lsh01087674117@gmail.com>
2022-03-17 14:18:31 +0900

    삼각 측량 방법을 적용하기 위해 2가지 또 다른 테스트 케이스 작성.

--- src/test/java/com/example/springboottdd/service/RatePointServiceTest.java
+++ src/test/java/com/example/springboottdd/service/RatePointServiceTest.java
@@ -28,4 +28,28 @@ public class RatePointServiceTest {
         // then
         assertThat(result).isEqualTo(100);
     }
+
+    @Test
+    public void _10000원의_적립은_200원(){
+        // given
+        final int price = 10000;
+
+        // when
+        final int result = ratePointService.calculateAmount(price);
+
+        // then
+        assertThat(result).isEqualTo(200);
+    }
+
+    @Test
+    public void _10000원의_적립은_300원(){
+        // given
+        final int price = 10000;
+
+        // when
+        final int result = ratePointService.calculateAmount(price);
+
+        // then
+        assertThat(result).isEqualTo(300);
+    }
 }
