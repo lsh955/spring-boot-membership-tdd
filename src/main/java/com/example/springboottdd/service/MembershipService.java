310bce31006e7c5a40c22770ebb1540207ce2d2b
이승환 <lsh01087674117@gmail.com>
2022-03-17 09:49:08 +0900

    removeMembership 프로덕션 코드 추가.

--- src/main/java/com/example/springboottdd/service/MembershipService.java
+++ src/main/java/com/example/springboottdd/service/MembershipService.java
@@ -71,4 +71,14 @@ public class MembershipService {
                 .createdAt(membership.getCreatedAt())
                 .build();
     }
+
+    public void removeMembership(final Long membershipId, final String userId) {
+        final Optional<Membership> optionalMembership = membershipRepository.findById(membershipId);
+        final Membership membership = optionalMembership.orElseThrow(() -> new MembershipException(MembershipErrorResult.MEMBERSHIP_NOT_FOUND));
+
+        if (!membership.getUserId().equals(userId))
+            throw new MembershipException(MembershipErrorResult.NOT_MEMBERSHIP_OWNER);
+
+        membershipRepository.deleteById(membershipId);
+    }
 }
