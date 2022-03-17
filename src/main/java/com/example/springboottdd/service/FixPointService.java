5335705bc12b96d113ccccb68de55a5d45e7db43
이승환 <lsh01087674117@gmail.com>
2022-03-17 14:26:36 +0900

    추후에 고정 금액 포인트 적립방식을 구현하기 위해 FixPointService를 미리 구현

---
+++ src/main/java/com/example/springboottdd/service/FixPointService.java
@@ -0,0 +1,17 @@
+package com.example.springboottdd.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 이승환
+ * @since 2022-03-17
+ */
+@Service
+public class FixPointService implements PointService {
+
+    @Override
+    public int calculateAmount(int price) {
+
+        return 0;
+    }
+}
