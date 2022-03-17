8375c7fca77751118c9018732f7d4db5340a3f3b
이승환 <lsh01087674117@gmail.com>
2022-03-17 14:12:16 +0900

    멤버십적립을 위한 포인트서비스 테스트 class 추가.

---
+++ src/test/java/com/example/springboottdd/service/RatePointServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.springboottdd.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author 이승환
+ * @since 2022-03-17
+ */
+@ExtendWith(MockitoExtension.class)
+public class RatePointServiceTest {
+
+
+}
