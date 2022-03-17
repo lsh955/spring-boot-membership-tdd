19b66efcf39486cfe1cd3e641f4a1f1cc6b74f29
이승환 <lsh01087674117@gmail.com>
2022-03-17 14:16:26 +0900

    calculateAmount 컴파일을 해결하기 위한 프로덕션 코드추가.

---
+++ src/main/java/com/example/springboottdd/service/RatePointService.java
@@ -0,0 +1,16 @@
+package com.example.springboottdd.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 이승환
+ * @since 2022-03-17
+ */
+@Service
+public class RatePointService {
+
+    public int calculateAmount(final int price) {
+
+        return 0;
+    }
+}
